c459f5fd71928613fac32895496bb2995ffc8e7b
Siva Dasu <sivadasu88@gmail.com>
2018-07-13 11:01:50 +0530

    User EF added

--- Toobtech.UserManagement.Common/User.cs
+++ Toobtech.UserManagement.Common/User.cs
@@ -7,6 +7,9 @@ namespace Toobtech.UserManagement.Common
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
     }
 }
